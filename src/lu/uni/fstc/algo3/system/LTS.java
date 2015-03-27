b28c90ce26c402b415dfa425e895a525dc346dda
GatisBee <GatisBee@users.noreply.github.com>
2015-03-27 21:58:26 +0100

    Update LTS.java
    Added a thought

--- src/lu/uni/fstc/algo3/system/LTS.java
+++ src/lu/uni/fstc/algo3/system/LTS.java
@@ -9,5 +9,13 @@ import java.util.Collection;
  */
 public class LTS {
     private RoadMap roadMap;
+    /** 
+    * This is the place where I imagine scans from all scanners could end up.
+    * And then different actors could query this collection. Since it is not constantly bothered with scan updates
+    * (scanners send their data once in some time period) it would make more sense to query this collection.
+    * On the other hand, it can happen that the road network is very large and many scanners want to send their data,
+    * so this collection could end up very bussy anyway. What kind of data struct we use and how we synchronize it will
+    * be very important.
+    */
     private Collection<ScanEntry> allScans;
 }
