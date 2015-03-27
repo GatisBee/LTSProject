27a2756861cdbb049e8d71335584d083e1759e1b
GatisBee <gatisbevalds@inbox.lv>
2015-03-27 16:04:22 +0100

    Small corections
    Some small corrections in interfaces and .gitignore file.

--- src/lu/uni/fstc/algo3/system/LTS.java
+++ src/lu/uni/fstc/algo3/system/LTS.java
@@ -1,8 +1,13 @@
 package lu.uni.fstc.algo3.system;
 
+import lu.uni.fstc.algo3.statistics.ScanEntry;
+
+import java.util.Collection;
+
 /**
  * Created by Gatis on 27/03/2015.
  */
 public class LTS {
     private RoadMap roadMap;
+    private Collection<ScanEntry> allScans;
 }
--- src/lu/uni/fstc/algo3/system/RoadSection.java
+++ src/lu/uni/fstc/algo3/system/RoadSection.java
@@ -6,15 +6,13 @@ import java.util.Collection;
  * Created by Gatis on 27/03/2015.
  */
 public class RoadSection {
-    private Collection<Scanner> scanners;
     private Collection<Checkpoint> checkpoints;
-    private static final byte MIN_CHECKPOINTS = 2;
 
-    protected boolean addScanner(Scanner scanner) {
+    protected boolean addCheckpoint(Checkpoint checkpoint) {
         return true;
     }
 
-    protected boolean removeScanner(Scanner scanner) {
+    protected boolean removeCheckpoint(Checkpoint checkpoint) {
         return true;
     }
 }
