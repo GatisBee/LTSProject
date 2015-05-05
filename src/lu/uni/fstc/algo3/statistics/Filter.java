31c3f51978f9d457891e16668d206c6458e42313
GatisBee <gatisbevalds@inbox.lv>
2015-05-05 09:55:11 +0200

    Hope this doesn't fuck up much.

--- src/lu/uni/fstc/algo3/statistics/Filter.java
+++ src/lu/uni/fstc/algo3/statistics/Filter.java
@@ -4,6 +4,7 @@ package lu.uni.fstc.algo3.statistics;
  * Provides some basic filtering operations on collections of type T.
  * Created by Gatis on 27/03/2015.
  */
-public class Filter<T> {
+public interface Filter<T> {
+	public boolean filter(T obj);
     //TODO: implement filtering operations, not urgent, but this will be a whole set of related tasks
 }
